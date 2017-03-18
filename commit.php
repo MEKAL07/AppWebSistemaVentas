b6c90901463e2129746ecc78a3b3ae08daec4b48
Wilson Morccolla Pampañaupa <wilson.libra112150@gmail.com>
2017-03-17 20:32:34 -0500

    cracion de archivo TUsuario en Model

---
+++ commit.php
@@ -0,0 +1,11 @@
+/*
+proyecto> git init //para iniciar el github
+
+proyecto> git add * //para añadir todas las fuentes al repositorio
+
+proyecto> git status //vemos que archivos se modificó
+
+proyecto> git commit -m "Subo la estructura del proyecto al repositorio de GitHub" //el commit
+proyecto> git remote add origin git@github.com:my_company/my_project.git //a donde lo vamos a subir
+proyecto> git push -u origin master //para empezar a subir los cambios
+*/
